dc4587ab71efd0067ce97f0b28ba66c104af4d8d
RadRussianRus <radrussianrus@gmail.com>
2018-12-26 14:26:08 +0300

    Extracted strings
    Helpful for translation.

--- servstatsbot.py
+++ servstatsbot.py
@@ -15,7 +15,28 @@ import telegram
 from telegram import ChatAction, ReplyMarkup
 from telegram.ext import Filters, Updater
 
-
+STRINGS = {
+    'button_stop': 'Stop',
+    'reply_stopped': 'All operations stopped.',
+    'reply_set_poll_interval': 'Send me a new polling interval in seconds? (higher than 10)',
+    'reply_set_poll_interval_done': 'All set!',
+    'reply_set_poll_interval_error': 'Please send a proper numeric value higher than 10.',
+    'reply_shell_cmd': 'Send me a shell command to execute',
+    'reply_shell_cmd_empty': 'No output.',
+    'reply_set_threshold': 'Send me a new memory threshold to monitor?',
+    'reply_set_threshold_done': 'All set!',
+    'reply_set_threshold_error': 'Please send a proper numeric value below 100.',
+    'graph_title': 'Memory Usage Graph',
+    'graph_x': 'Last %.2f hours',
+    'graph_y': '% Used',
+    'graph_threshold': 'Threshold: {} %',
+    'stats_onilne_hours': 'Online for: {:.1f} Hours',
+    'stats_memory_total': 'Total memory: {:.2f} GB ',
+    'stats_memory_available': 'Available memory: {:.2f} GB',
+    'stats_memory_used': 'Used memory: {} %',
+    'stats_disk_used': 'Disk used: {} %',
+    'alert_low_memory': 'CRITICAL! LOW MEMORY!'
+}
 
 memorythreshold = 85  # If memory usage more this %
 poll = 300  # seconds
@@ -28,7 +49,7 @@ settingmemth = []
 setpolling = []
 graphstart = datetime.now()
 
-stopmarkup = {'keyboard': [['Stop']]}
+stopmarkup = {'keyboard': [[STRINGS['button_stop']]]}
 hide_keyboard = {'hide_keyboard': True}
 
 def clearall(chat_id):
@@ -43,9 +64,9 @@ def plotmemgraph(memlist, xaxis, tmperiod):
     # print(memlist)
     # print(xaxis)
     plt.xlabel(tmperiod)
-    plt.ylabel('% Used')
-    plt.title('Memory Usage Graph')
-    plt.text(0.1*len(xaxis), memorythreshold+2, 'Threshold: '+str(memorythreshold)+ ' %')
+    plt.ylabel(STRINGS['graph_y'])
+    plt.title(STRINGS['graph_title'])
+    plt.text(0.1*len(xaxis), memorythreshold+2, STRINGS['graph_threshold'].format(memorythreshold))
     memthresholdarr = []
     for xas in xaxis:
         memthresholdarr.append(memorythreshold)
@@ -71,11 +92,11 @@ def on_message(bot, upd):
         disk = psutil.disk_usage('/')
         boottime = datetime.fromtimestamp(psutil.boot_time())
         now = datetime.now()
-        timedif = "Online for: %.1f Hours" % (((now - boottime).total_seconds()) / 3600)
-        memtotal = "Total memory: %.2f GB " % (memory.total / 1000000000)
-        memavail = "Available memory: %.2f GB" % (memory.available / 1000000000)
-        memuseperc = "Used memory: " + str(memory.percent) + " %"
-        diskused = "Disk used: " + str(disk.percent) + " %"
+        timedif = STRINGS['stats_onilne_hours'].format(((now - boottime).total_seconds()) / 3600)
+        memtotal = STRINGS['stats_memory_total'].format(memory.total / 1000000000)
+        memavail = STRINGS['stats_memory_available'].format(memory.available / 1000000000)
+        memuseperc = STRINGS['stats_memory_used'].format(memory.percent)
+        diskused = STRINGS['stats_disk_used'].format(disk.percent)
         pids = psutil.pids()
         pidsreply = ''
         procs = {}
@@ -100,44 +121,44 @@ def on_message(bot, upd):
                 diskused + "\n\n" + \
                 pidsreply
         bot.send_message(chat_id, reply, disable_web_page_preview=True)
-    elif message == "Stop":
+    elif message == STRINGS['button_stop']:
         clearall(chat_id)
-        bot.send_message(chat_id, "All operations stopped.", reply_markup=hide_keyboard)
+        bot.send_message(chat_id, STRINGS['reply_stopped'], reply_markup=hide_keyboard)
     elif message == '/setpoll' and chat_id not in setpolling:
         bot.send_chat_action(chat_id, ChatAction.TYPING)
         setpolling.append(chat_id)
-        bot.send_message(chat_id, "Send me a new polling interval in seconds? (higher than 10)", reply_markup=stopmarkup)
+        bot.send_message(chat_id, STRINGS['reply_set_poll_interval'], reply_markup=stopmarkup)
     elif chat_id in setpolling:
         bot.send_chat_action(chat_id, ChatAction.TYPING)
         try:
             global poll
             poll = int(message)
             if poll > 10:
-                bot.send_message(chat_id, "All set!")
+                bot.send_message(chat_id, STRINGS['reply_set_poll_interval_done'])
                 clearall(chat_id)
             else:
                 1/0
         except:
-            bot.send_message(chat_id, "Please send a proper numeric value higher than 10.")
+            bot.send_message(chat_id, STRINGS['reply_set_poll_interval_error'])
     elif message == "/shell" and chat_id not in shellexecution:
-        bot.send_message(chat_id, "Send me a shell command to execute", reply_markup=stopmarkup)
+        bot.send_message(chat_id, STRINGS['reply_shell_cmd'], reply_markup=stopmarkup)
         shellexecution.append(chat_id)
     elif message == "/setmem" and chat_id not in settingmemth:
         bot.send_chat_action(chat_id, ChatAction.TYPING)
         settingmemth.append(chat_id)
-        bot.send_message(chat_id, "Send me a new memory threshold to monitor?", reply_markup=stopmarkup)
+        bot.send_message(chat_id, STRINGS['reply_set_threshold'], reply_markup=stopmarkup)
     elif chat_id in settingmemth:
         bot.send_chat_action(chat_id, ChatAction.TYPING)
         try:
             global memorythreshold
             memorythreshold = int(message)
             if memorythreshold < 100:
-                bot.send_message(chat_id, "All set!")
+                bot.send_message(chat_id, STRINGS['reply_set_threshold_done'])
                 clearall(chat_id)
             else:
                 1/0
         except:
-            bot.send_message(chat_id, "Please send a proper numeric value below 100.")
+            bot.send_message(chat_id, STRINGS['reply_set_threshold_error'])
 
     elif chat_id in shellexecution:
         bot.send_chat_action(chat_id, ChatAction.TYPING)
@@ -146,10 +167,10 @@ def on_message(bot, upd):
         if output != b'':
             bot.send_message(chat_id, output, disable_web_page_preview=True)
         else:
-            bot.send_message(chat_id, "No output.", disable_web_page_preview=True)
+            bot.send_message(chat_id, STRINGS['reply_shell_cmd_empty'], disable_web_page_preview=True)
     elif message == '/memgraph':
         bot.send_chat_action(chat_id, ChatAction.TYPING)
-        tmperiod = "Last %.2f hours" % ((datetime.now() - graphstart).total_seconds() / 3600)
+        tmperiod = STRINGS['graph_x'].format((datetime.now() - graphstart).total_seconds() / 3600)
         bot.sendPhoto(chat_id, plotmemgraph(memlist, xaxis, tmperiod))
 
 
@@ -180,11 +201,11 @@ while 1:
             memlist.append(mempercent)
         memfree = memck.available / 1000000
         if mempercent > memorythreshold:
-            memavail = "Available memory: %.2f GB" % (memck.available / 1000000000)
+            memavail = STRINGS['stats_memory_available'].format(memck.available / 1000000000)
             graphend = datetime.now()
-            tmperiod = "Last %.2f hours" % ((graphend - graphstart).total_seconds() / 3600)
+            tmperiod = STRINGS['graph_x'].format((graphend - graphstart).total_seconds() / 3600)
             for adminid in adminchatid:
-                updater.bot.send_message(adminid, "CRITICAL! LOW MEMORY!\n" + memavail)
+                updater.bot.send_message(adminid, "{}\n{}".format(STRINGS['alert_low_memory'], memavail)
                 updater.bot.send_photo(adminid, plotmemgraph(memlist, xaxis, tmperiod))
     time.sleep(10)  # 10 seconds
     tr += 10
